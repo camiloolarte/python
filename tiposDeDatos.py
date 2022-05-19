ae7c19e3763feb7784bd9da161c805c30fcee3ac
Cristhian Camilo Olarte Carvajal <graficolarte@gmail.com>
2022-05-18 20:39:49 -0500

    se termina de agregar los ejemplos de datos string

--- tiposDeDatos.py
+++ tiposDeDatos.py
@@ -1,21 +1,43 @@
 # tipos de datos python
 
-#string
-print('"Isn\'t," they said.')
+# ----------------------- STRINGS -------------------
+
+print('"Isn\'t," they said. #tilde dentro de comillas')
 
 s = 'First line.\nSecond line.'
-print(s)
+print(s+' #salto de linea')
 
-print(r'C:\some\name') #cadenas sin formato
+print(r'C:\some\name'+' # linea sin formato') #cadenas sin formato
 
 print("""
 Usage: thingy [OPTIONS]
      -h                        Display this usage message
      -H hostname               Hostname to connect to
+     #multiple lineas
 """)
 
-print (3*'mi' + 'ma')
+print (3*'mi' + 'ma' +' #multiplica strings')
 
 word = 'Python'
-print(word[5], word[-4], word[0:2], word[2:5])  # character in position 0) 
+print("""
+ +---+---+---+---+---+---+
+ | P | y | t | h | o | n |
+ +---+---+---+---+---+---+
+ 0   1   2   3   4   5   6
+-6  -5  -4  -3  -2  -1
+
+"""
+)
+print(word[5]+ '(5)', word[-4]+'(-4)', word[0:2]+'(0:2)', word[2:5]+'(2:5)')  # character in position 0) 
+
+print(word[:2]+' (:2)')
+print(word[4:]+' (4:)')
+print(word[-2:]+' (-2)')
+print(word[:2] + word[2:] + '(:2)+(2:)')
+print(word[:4] + word[4:]+' (:4)+(4:)')
+
+print('J' + word[1:]+' J+word[1:] #para asignar un caracter dentro d euna cadena de debe creae una nueva')
+
+#----------------------------LISTAS------------------------------
+
 
